1073643c94473b3315ae546614a54364456ef653
mccrush <mccrush2027@gmail.com>
2019-05-29 18:54:02 +0700

    Удаляет неиспользуемые компоненты

--- src/router.js
+++ src/router.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
-// import App from './views/App.vue'
-// import Edu from './views/Edu.vue'
-// import Gtd from './views/Gtd.vue'
 import Page from './views/Page.vue'
 
 Vue.use(Router)
@@ -17,26 +13,9 @@ export default new Router({
       name: 'page',
       component: Page
     },
-    { path: '/', redirect: '/home' }
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/app',
-    //   name: 'app',
-    //   component: App
-    // },
-    // {
-    //   path: '/edu',
-    //   name: 'edu',
-    //   component: Edu
-    // },
-    // {
-    //   path: '/gtd',
-    //   name: 'gtd',
-    //   component: Gtd
-    // }
+    {
+      path: '/',
+      redirect: '/home'
+    }
   ]
 })
