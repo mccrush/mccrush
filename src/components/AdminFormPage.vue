2e3dd1dafb03b8f93bd3b53f0623a01e7d2312df
mccrush <mccrush2027@gmail.com>
2019-05-31 19:56:59 +0700

    Настраивает отображение кнопок

--- src/components/AdminFormPage.vue
+++ src/components/AdminFormPage.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="row">
-    <div class="col">tecData {{createFullDate()}}</div>
+    <div class="col">
+      <form>
+        <div class="form-group">
+          <label for="itemTitle">Заголовок страницы</label>
+          <input type="text" class="form-control" id="itemTitle" aria-describedby="titleHelp" placeholder="Введите заголовок страницы" maxlength="20" v-model="itemObj.title" @focus="editingForm()" @blur="saveForm()">
+          <small id="titleHelp" class="form-text text-muted">Длина не более 20 символов</small>
+        </div>
+        <div class="form-group">
+          <label for="itemDescription">Описание страницы</label>
+          <textarea class="form-control" id="itemDescription" aria-describedby="descriptionHelp" placeholder="Введите краткое описание страницы" rows="3" v-model="itemObj.description" @focus="editingForm()" @blur="saveForm()"></textarea>
+          <small id="descriptionHelp" class="form-text text-muted">Лучше - короче</small>
+        </div>
+        <div class="form-group">
+          <label for="itemContent">Контент страницы</label>
+          <textarea class="form-control" id="itemContent" aria-describedby="contentHelp" placeholder="Заполните содержимое страницы" rows="5" v-model="itemObj.content" @focus="editingForm()" @blur="saveForm()"></textarea>
+          <small id="contentHelp" class="form-text text-muted">Как в Worde</small>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            <button class="btn btn-block btn-light" @click.prevent="clearForm">Очистить поля</button>
+          </div>
+          <div class="col-6">
+            <button class="btn btn-block btn-success" @click.prevent="saveForm">{{buttonSave}}</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -10,28 +36,40 @@ export default {
   data() {
     return {
       dateNow: new Date(),
-      formDate: "",
-      email: "Показать email"
+      itemObj: {
+        title: "",
+        description: "",
+        content: "",
+        dateUpdate: Date().toString()
+      },
+      buttonSave: "Сохранено"
     };
   },
   created() {
     //this.tecYear = new Date().getFullYear();
+    console.log("date:", this.itemObj.dateUpdate);
   },
   methods: {
-    createFullDate() {
-      let dd =
-        this.dateNow.getDate() < 10
-          ? "0" + this.dateNow.getDate()
-          : this.dateNow.getDate();
-      let mm =
-        this.dateNow.getMonth() + 1 < 10
-          ? "0" + (this.dateNow.getMonth() + 1)
-          : this.dateNow.getMonth() + 1;
-      let yyyy = this.dateNow.getFullYear();
-      return (this.dateNow = dd + "." + mm + "." + yyyy);
+    // createFullDate() {
+    //   let dd =
+    //     this.dateNow.getDate() < 10
+    //       ? "0" + this.dateNow.getDate()
+    //       : this.dateNow.getDate();
+    //   let mm =
+    //     this.dateNow.getMonth() + 1 < 10
+    //       ? "0" + (this.dateNow.getMonth() + 1)
+    //       : this.dateNow.getMonth() + 1;
+    //   let yyyy = this.dateNow.getFullYear();
+    //   return (this.dateNow = dd + "." + mm + "." + yyyy);
+    // }
+    clearForm() {},
+    editingForm() {
+      this.buttonSave = "Сохранить";
+      return this.buttonSave;
     },
-    showEmail() {
-      this.email = "mccrush2027@gmail.com";
+    saveForm() {
+      this.buttonSave = "Сохранено";
+      return this.buttonSave;
     }
   }
 };
