1e82b5584a8fabcbcbb60c68cb852a22af217842
mccrush <mccrush2027@gmail.com>
2019-06-06 12:17:50 +0700

    Скрывает отображение версии

--- src/components/AppFoot.vue
+++ src/components/AppFoot.vue
@@ -5,9 +5,9 @@
         <p>
           &copy; {{tecYear}} mccrush.ru&nbsp;&nbsp;
           <small>|</small>&nbsp;&nbsp;
-          <a href="https://github.com/mccrush/mccrush/releases" target="_blank">v2.0.1</a>&nbsp;
-          <small class="text-muted">от 06.06.2019</small>&nbsp;&nbsp;
-          <small>|</small>&nbsp;&nbsp;
+          <!-- <a href="https://github.com/mccrush/mccrush/releases" target="_blank">v2.0.1</a>&nbsp;
+          <small class="text-muted">от 06.06.2019</small>&nbsp;&nbsp; 
+          <small>|</small>&nbsp;&nbsp;-->
           <span class="showem" @click="showEmail">{{email}}</span>
         </p>
       </footer>
