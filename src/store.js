d1b22b330fc10b7e96092b8142be2af8b2fee2d0
mccrush <mccrush2027@gmail.com>
2019-06-07 21:30:11 +0700

    Настраивает получение данных

--- src/store.js
+++ src/store.js
@@ -14,29 +14,74 @@ export default new Vuex.Store({
   },
   mutations: {
     getArr(state, typeCol) {
-      db.collection(typeCol)
-        .orderBy("posmenu")
-        .get()
-        .then(querySnapshot => {
-          if (typeCol == 'page') {
-            querySnapshot.forEach(doc => {
-              state.pageId.push(doc.id);
-              state.pageArr.push(doc.data());
-              //console.log(`${doc.id} => ${doc.data().alias}`);
+
+      if (typeCol == 'page') {
+        if (localStorage.getItem('pageId')) {
+          state.pageId = JSON.parse(localStorage.getItem('pageId'));
+          state.pageArr = JSON.parse(localStorage.getItem('pageArr'));
+        } else {
+          db.collection(typeCol)
+            .orderBy("posmenu")
+            .get()
+            .then(querySnapshot => {
+              querySnapshot.forEach(doc => {
+                state.pageId.push(doc.id);
+                state.pageArr.push(doc.data());
+              });
+              console.log("in store this.pageArr:", this.pageArr);
+            })
+            .catch(error => {
+              console.log(error);
             });
-          } else if (typeCol == 'app') {
-            querySnapshot.forEach(doc => {
-              state.appId.push(doc.id);
-              state.appArr.push(doc.data());
-              //console.log(`${doc.id} => ${doc.data().alias}`);
+        }
+      } else if (typeCol == 'app') {
+        if (localStorage.getItem('appId')) {
+          state.appId = JSON.parse(localStorage.getItem('appId'));
+          state.appArr = JSON.parse(localStorage.getItem('appArr'));
+        } else {
+          db.collection(typeCol)
+            .orderBy("posmenu")
+            .get()
+            .then(querySnapshot => {
+              querySnapshot.forEach(doc => {
+                state.appId.push(doc.id);
+                state.appArr.push(doc.data());
+              });
+              console.log("in store this.appArr:", this.appArr);
+            })
+            .catch(error => {
+              console.log(error);
             });
-          }
-          //console.log("this.pageArr:", this.pageArr);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-      //console.log("after mutations, store.state.authStatus:", store.state.authStatus);
+        }
+      }
+
+
+
+
+      // if (localStorage.getItem('pageId').length >= 1) {
+      //   state.pageId = JSON.parse(localStorage.getItem('pageId'));
+      //   state.pageArr = JSON.parse(localStorage.getItem('pageArr'));
+      // } else {
+      //   db.collection(typeCol)
+      //     .orderBy("posmenu")
+      //     .get()
+      //     .then(querySnapshot => {
+      //       if (typeCol == 'page') {
+      //         querySnapshot.forEach(doc => {
+      //           state.pageId.push(doc.id);
+      //           state.pageArr.push(doc.data());
+      //         });
+      //       } else if (typeCol == 'app') {
+      //         querySnapshot.forEach(doc => {
+      //           state.appId.push(doc.id);
+      //           state.appArr.push(doc.data());
+      //         });
+      //       }
+      //     })
+      //     .catch(error => {
+      //       console.log(error);
+      //     });
+      // }
     },
     setItem(state, payload) {
       payload.id = payload.id ? payload.id : db.collection(payload.col).doc().id;
