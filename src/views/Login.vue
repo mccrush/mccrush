fccaa0a2999f7ea130567b1d210a45be05dcbb18
mccrush <mccrush2027@gmail.com>
2019-11-20 20:27:45 +0700

    Переименовывает файл

---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="row home justify-content-center">
+    <div class="col-4 text-left">
+      <h4>Авторизируйтесь для продолжения</h4>
+      <label for="email">Email:</label>
+      <br />
+      <input type="text" class="form-control" id="email" v-model="email" />
+      <br />
+      <label for="password">Password:</label>
+      <input type="password" class="form-control" id="password" v-model="password" />
+      <br />
+      <div class="row">
+        <div class="col-6">
+          <button class="btn btn-success btn-block" @click="signin">Регистрация</button>
+        </div>
+        <div class="col-6">
+          <button class="btn btn-success btn-block" @click="login">Вход</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { auth } from "@/main.js";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      auth
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$store.commit("setUser");
+          this.$router.push("/about");
+        })
+        .catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert("Login: errors:", errorCode, "& ", errorMessage);
+        });
+    },
+
+    signin() {
+      auth
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$store.commit("setUser");
+          this.$router.push("/about");
+        })
+        .catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert("Signin: errors:", errorCode, "& ", errorMessage);
+        });
+    }
+  }
+};
+</script>
