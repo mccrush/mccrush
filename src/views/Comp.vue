462f484bbd8f42c2465f107ea5e6a40b1f73fc22
mccrush <mccrush2027@gmail.com>
2020-01-19 13:10:50 +0700

    Настройка карточек

--- src/views/Comp.vue
+++ src/views/Comp.vue
@@ -5,9 +5,12 @@
         <div class="card-header bg-secondary border-0 rounded-top">
           <h5 class="text-white text-center m-0">Base</h5>
         </div>
-        <div class="card-body">
+        <div class="card-body text-center">
           <h5 class="card-title">Установка Windows</h5>
-          <p class="card-text">+ активация</p>
+          <h5>7 или 10</h5>
+          <h5>+</h5>
+          <h5>Установка драйверов</h5>
+          <p class="card-text">звук, видео, сеть</p>
         </div>
         <div class="card-footer bg-secondary border-0">
           <h5 class="text-white text-center m-0">350P</h5>
@@ -17,9 +20,14 @@
         <div class="card-header bg-success border-0 rounded-top">
           <h5 class="text-white text-center m-0">Standart</h5>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <div class="card-body text-center">
+          <h5 class="bg-secondary text-white p-2 rounded">Пакет Base</h5>
+          <h5>+</h5>
+          <h5>Microsoft Office</h5>
+          <p class="card-text">Word, Excel, PowerPoint</p>
+          <h5>+</h5>
+          <h5>Установка браузера</h5>
+          <p class="card-text">Chrome | Opera | Firefox</p>
         </div>
         <div class="card-footer bg-success mb-sm-n4 border-0 shadow rounded-bottom">
           <h5 class="text-white text-center m-0">500P</h5>
@@ -29,9 +37,12 @@
         <div class="card-header bg-warning border-0 rounded-top">
           <h5 class="text-white text-center m-0">Premium</h5>
         </div>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <div class="card-body text-center">
+          <h5 class="bg-success text-white p-2 rounded">Пакет Standart</h5>
+          <h5>+</h5>
+          <h5>Ускорение загрузки</h5>
+          <h5>+</h5>
+          <h5>Гарантия 3 мес.</h5>
         </div>
         <div class="card-footer bg-warning border-0">
           <h5 class="text-white text-center m-0">750P</h5>
