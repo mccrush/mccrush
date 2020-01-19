521e94fe1fc435a1114d71e8a04e1e1a9fc17a6a
mccrush <mccrush2027@gmail.com>
2020-01-19 12:24:44 +0700

    Настраивает внешний вид карточек

--- src/views/Comp.vue
+++ src/views/Comp.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="row justify-content-center">
     <div class="card-group mt-4 mb-4 p-3">
-      <div class="card border-0 shadow card-base">
-        <div class="card-header bg-secondary border-0">
+      <div class="card border-0 shadow card-base rounded-top">
+        <div class="card-header bg-secondary border-0 rounded-top">
           <h5 class="text-white text-center m-0">Base</h5>
         </div>
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">Установка Windows</h5>
+          <p class="card-text">+ активация</p>
         </div>
         <div class="card-footer bg-secondary border-0">
           <h5 class="text-white text-center m-0">350P</h5>
         </div>
       </div>
-      <div class="card border-0 shadow mt-4 card-standart">
-        <div class="card-header bg-success border-0">
+      <div class="card border-0 shadow mt-sm-4 card-standart ml-n1 mr-n1 rounded-top">
+        <div class="card-header bg-success border-0 rounded-top">
           <h5 class="text-white text-center m-0">Standart</h5>
         </div>
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
         </div>
-        <div class="card-footer bg-success mb-n4 border-0 shadow">
+        <div class="card-footer bg-success mb-sm-n4 border-0 shadow rounded-bottom">
           <h5 class="text-white text-center m-0">500P</h5>
         </div>
       </div>
-      <div class="card border-0 shadow card-premium">
-        <div class="card-header bg-warning border-0">
+      <div class="card border-0 shadow card-premium rounded-top">
+        <div class="card-header bg-warning border-0 rounded-top">
           <h5 class="text-white text-center m-0">Premium</h5>
         </div>
         <div class="card-body">
