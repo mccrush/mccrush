c310ce2102e3ffd3043ef57aa083790f76a8c97d
mccrush <mccrush2027@gmail.com>
2020-04-21 19:20:29 +0700

    Добавляет структуру технологий

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,8 +10,8 @@
       </p>
       <hr />
 
-      <h5 style="text-align: center;">Стек технологий</h5>
-      <table class="table table-borderless table-sm mt-3" style="width: 100%;">
+      <h5 class="mb-3 text-center">Стек технологий</h5>
+      <table class="table table-borderless table-sm" style="width: 100%;">
         <tbody>
           <tr>
             <td style="width: 26%; text-align: center; vertical-align: middle;">
@@ -45,12 +45,52 @@
           </tr>
         </tbody>
       </table>
-      <div class="row">
-        <div class="col">
-          <h4>&nbsp;</h4>
+      <h5 class="mt-5 mb-3 text-center">Структура технологий</h5>
+      <div class="row m-0">
+        <div class="col-12">
+          <div class="row row-tech-50">
+            <div class="col-12 shadow-sm rounded-sm box-tech">PWA</div>
+          </div>
+          <div class="row">
+            <div class="col-4 shadow-sm rounded-sm box-tech">Bootstrap</div>
+            <div class="col-4">
+              <div class="row row-tech-50">
+                <div class="col-12 shadow-sm rounded-sm box-tech">Vue.js</div>
+              </div>
+              <div class="row row-tech-50">
+                <div class="col-6 shadow-sm rounded-sm box-tech">Vuex</div>
+                <div class="col-6 shadow-sm rounded-sm box-tech">Router</div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="row row-tech-50">
+                <div class="col-12 shadow-sm rounded-sm box-tech">Firebase</div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <div class="row row-tech-50">
+                    <div class="col-12 shadow-sm rounded-sm small box-tech">Authentication</div>
+                    <div class="col-12 shadow-sm rounded-sm small box-tech">Cloud Storege</div>
+                  </div>
+                </div>
+                <div class="col-6">
+                  <div class="row row-tech-50">
+                    <div class="col-12 shadow-sm rounded-sm small box-tech">Cloud Firestore</div>
+                    <div class="col-12 shadow-sm rounded-sm small box-tech">Hosting</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row row-tech-50">
+            <div class="col-2 shadow-sm rounded-sm box-tech">HTML</div>
+            <div class="col-2 shadow-sm rounded-sm box-tech">CSS</div>
+            <div class="col-8 shadow-sm rounded-sm box-tech">JavaScript</div>
+          </div>
         </div>
       </div>
       <hr />
+      <h5 class="mb-3 text-center">Разделы сайта</h5>
       <div class="row mr-0 ml-0">
         <router-link class="col-12 col-sm-4 shadow-sm rounded box text-decoration-none text-center pt-4" to="/app">
           <img src="img/home/icons_box/app.png" width="56" height="56" alt="Приложения" />
@@ -105,11 +145,35 @@ export default {
   color: #212529;
   /* border-top: 3px solid rgba(255, 0, 0, 0.5); */
   /* border-bottom: 2px solid white; */
-  transition: 0.5s;
+  transition: 0.3s;
 }
 .box:hover {
   box-shadow: 0 0.5rem 1rem rgba(256, 0, 0, 0.15) !important;
   /* border-top: 3px solid rgba(255, 0, 0, 0.75); */
   /* color: rgba(196, 0, 0, 1); */
+}
+
+.row-tech-50 {
+  height: 50px;
+  margin-top: 4px;
+}
+
+.row-tech-100 {
+  height: 100px;
+  margin-top: 4px;
+}
+
+.box-tech {
+  cursor: default;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 2px solid rgba(224, 0, 0, 0.55);
+  transition: 0.3s;
+}
+
+.box-tech:hover {
+  box-shadow: 0 0.3rem 0.7rem rgba(256, 0, 0, 0.15) !important;
+  /* border: solid 1px rgba(255, 0, 0, 0.5); */
 }
 </style>
