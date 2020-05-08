e570236943925709f8c95a039399da4266e0692b
mccrush <mccrush2027@gmail.com>
2020-05-08 17:44:33 +0700

    Добавляет редакторы

--- src/pages/Dev.vue
+++ src/pages/Dev.vue
@@ -5,125 +5,123 @@
       <h3>Разработка</h3>
       <p>Ссылки на качественные материалы по веб-разработке. Данная информация бедет полезна не только веб-разработчикам, но и тем, кто желает расширить свой кругозор</p>
       <hr />
-    </div>
 
-    <div class="row pl-3 pr-3">
-      <div class="col-12 col-sm-6 mb-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">Документация основы</h5>
-            <ul class="pl-4 mb-1">
-              <li>
-                <a href="http://htmlbook.ru/html" target="_blank" title="Открыть сайт">HTML</a>
-              </li>
-              <li>
-                <a href="http://htmlbook.ru/css" target="_blank" title="Открыть сайт">CSS</a>
-              </li>
-              <li>
-                <a href="https://developer.mozilla.org/ru/docs/Web/JavaScript" target="_blank" title="Открыть сайт">JavaScript | MDN</a>
-              </li>
-            </ul>
+      <div class="row pl-3 pr-3">
+        <div class="col-12 col-sm-6 mb-4">
+          <div class="card shadow-sm red-border">
+            <div class="card-body">
+              <h5 class="card-title">Документация основы</h5>
+              <ul class="pl-4 mb-1">
+                <li>
+                  <a href="http://htmlbook.ru/html" target="_blank" title="Открыть сайт">HTML</a>
+                </li>
+                <li>
+                  <a href="http://htmlbook.ru/css" target="_blank" title="Открыть сайт">CSS</a>
+                </li>
+                <li>
+                  <a href="https://developer.mozilla.org/ru/docs/Web/JavaScript" target="_blank" title="Открыть сайт">JavaScript | MDN</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-sm-6 mb-4">
+          <div class="card shadow-sm red-border">
+            <div class="card-body">
+              <h5 class="card-title">Документация технологии</h5>
+              <ul class="pl-4 mb-1">
+                <li>
+                  <a href="https://getbootstrap.com/" target="_blank" title="Открыть сайт">Bootstrap</a>
+                </li>
+                <li>
+                  <a href="https://vuejs.org/" target="_blank" title="Открыть сайт">Vue.js</a>
+                </li>
+                <li>
+                  <a href="https://firebase.google.com/docs?hl=ru" target="_blank" title="Открыть сайт">Firebase</a>
+                </li>
+                <li>
+                  <a href="https://codelabs.developers.google.com/codelabs/your-first-pwapp/?hl=ru#0" target="_blank" title="Открыть сайт">PWA (Progressive Web App)</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-sm-6 mb-4">
+          <div class="card shadow-sm red-border">
+            <div class="card-body">
+              <h5 class="card-title">Онлайн учебники</h5>
+              <ul class="pl-4 mb-1">
+                <li>
+                  <a href="https://learn.javascript.ru/" target="_blank" title="Открыть сайт">Учебник JavaScript</a>
+                </li>
+                <li>
+                  <a href="https://github.com/azat-io/you-dont-know-js-ru" target="_blank" title="Открыть сайт">"Вы не знаете JS"</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-sm-6 mb-4">
+          <div class="card shadow-sm red-border">
+            <div class="card-body">
+              <h5 class="card-title">Онлайн курсы</h5>
+              <ul class="pl-4 mb-1">
+                <li>
+                  <a href="https://ru.hexlet.io/?ref=107231" target="_blank" title="Открыть сайт">hexlet.io</a>
+                </li>
+                <li>
+                  <a href="https://www.codecademy.com/learn" target="_blank" title="Открыть сайт">codecademy.com (en)</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-sm-6 mb-4">
+          <div class="card shadow-sm red-border">
+            <div class="card-body">
+              <h5 class="card-title">Списки книг</h5>
+              <ul class="pl-4 mb-1">
+                <li>
+                  <a title="Открыть сайт" href="https://ru.hexlet.io/pages/recommended-books/?ref=107231" target="_blank">Список hexlet.io</a>
+                </li>
+                <li>
+                  <a title="Открыть сайт" href="https://learn.javascript.ru/books" target="_blank">Список learn.javascript.ru</a>
+                </li>
+                <li>
+                  <a title="Открыть сайт" href="https://stackoverflow.com/questions/1711/what-is-the-single-most-influential-book-every-programmer-should-read" target="_blank">stackoverflow.com (en)</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-sm-6 mb-4">
+          <div class="card shadow-sm red-border">
+            <div class="card-body">
+              <h5 class="card-title">YouTube каналы</h5>
+              <ul class="pl-4 mb-1">
+                <li>
+                  <a href="https://www.youtube.com/channel/UCg8ss4xW9jASrqWGP30jXiw/playlists" target="_blank" title="Открыть сайт">Владилен Минин</a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/user/ArtSorax/playlists" target="_blank" title="Открыть сайт">Sorax</a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/user/iliakan/playlists" target="_blank" title="Открыть сайт">JavaScript.ru</a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/user/agragregra/playlists" target="_blank" title="Открыть сайт">WebDesign Master</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="col-12 col-sm-6 mb-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">Документация технологии</h5>
-            <ul class="pl-4 mb-1">
-              <li>
-                <a href="https://getbootstrap.com/" target="_blank" title="Открыть сайт">Bootstrap</a>
-              </li>
-              <li>
-                <a href="https://vuejs.org/" target="_blank" title="Открыть сайт">Vue.js</a>
-              </li>
-              <li>
-                <a href="https://firebase.google.com/docs?hl=ru" target="_blank" title="Открыть сайт">Firebase</a>
-              </li>
-              <li>
-                <a href="https://codelabs.developers.google.com/codelabs/your-first-pwapp/?hl=ru#0" target="_blank" title="Открыть сайт">PWA (Progressive Web App)</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-sm-6 mb-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">Онлайн учебники</h5>
-            <ul class="pl-4 mb-1">
-              <li>
-                <a href="https://learn.javascript.ru/" target="_blank" title="Открыть сайт">Учебник JavaScript</a>
-              </li>
-              <li>
-                <a href="https://github.com/azat-io/you-dont-know-js-ru" target="_blank" title="Открыть сайт">"Вы не знаете JS"</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-sm-6 mb-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">Онлайн курсы</h5>
-            <ul class="pl-4 mb-1">
-              <li>
-                <a href="https://ru.hexlet.io/?ref=107231" target="_blank" title="Открыть сайт">hexlet.io</a>
-              </li>
-              <li>
-                <a href="https://www.codecademy.com/learn" target="_blank" title="Открыть сайт">codecademy.com (en)</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-sm-6 mb-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">Списки книг</h5>
-            <ul class="pl-4 mb-1">
-              <li>
-                <a title="Открыть сайт" href="https://ru.hexlet.io/pages/recommended-books/?ref=107231" target="_blank">Список hexlet.io</a>
-              </li>
-              <li>
-                <a title="Открыть сайт" href="https://learn.javascript.ru/books" target="_blank">Список learn.javascript.ru</a>
-              </li>
-              <li>
-                <a title="Открыть сайт" href="https://stackoverflow.com/questions/1711/what-is-the-single-most-influential-book-every-programmer-should-read" target="_blank">stackoverflow.com (en)</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-sm-6 mb-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h5 class="card-title">YouTube каналы</h5>
-            <ul class="pl-4 mb-1">
-              <li>
-                <a href="https://www.youtube.com/channel/UCg8ss4xW9jASrqWGP30jXiw/playlists" target="_blank" title="Открыть сайт">Владилен Минин</a>
-              </li>
-              <li>
-                <a href="https://www.youtube.com/user/ArtSorax/playlists" target="_blank" title="Открыть сайт">Sorax</a>
-              </li>
-              <li>
-                <a href="https://www.youtube.com/user/iliakan/playlists" target="_blank" title="Открыть сайт">JavaScript.ru</a>
-              </li>
-              <li>
-                <a href="https://www.youtube.com/user/agragregra/playlists" target="_blank" title="Открыть сайт">WebDesign Master</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-12">
       <h5>Цикл видео о сетевых технологиях от Дмитрия Бачило</h5>
       <p>
         Сайт:
@@ -135,6 +133,41 @@
         Сайт: страница на
         <a href="https://github.com/andrew--r/channels" target="_blank">GitHub</a>
       </p>
+      <hr />
+
+      <h5 class="mb-4">Редакторы для комфортного программирования</h5>
+      <div class="row mb-2">
+        <div class="col-12 col-sm-4">
+          <div class="card shadow">
+            <img src="/img/dev/vscode.jpg" class="card-img-top" alt="Visual Studio Code" />
+            <div class="card-body">
+              <h5 class="card-title">VS Code</h5>
+              <p class="card-text">Free. Built on open source. Runs everywhere</p>
+              <a href="https://code.visualstudio.com/" class="btn btn-block btn-sm btn-primary" target="_blank">Перейти</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-4">
+          <div class="card shadow">
+            <img src="/img/dev/storm.jpg" class="card-img-top" alt="WebStorm" />
+            <div class="card-body">
+              <h5 class="card-title">WebStorm</h5>
+              <p class="card-text">The smartest JavaScript IDE</p>
+              <a href="https://www.jetbrains.com/webstorm/" class="btn btn-block btn-sm btn-primary" target="_blank">Перейти</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-4">
+          <div class="card shadow">
+            <img src="/img/dev/sublime.jpg" class="card-img-top" alt="Sublime Text 3" />
+            <div class="card-body">
+              <h5 class="card-title">Sublime Text</h5>
+              <p class="card-text">A sophisticated text editor for code, markup and prose</p>
+              <a href="https://www.sublimetext.com/" class="btn btn-block btn-sm btn-primary" target="_blank">Перейти</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -153,6 +186,8 @@ export default {
 <style scoped>
 .card {
   border: none;
+}
+.red-border {
   border-top: 3px solid #ea5455 /*rgba(224, 0, 0, 0.75)*/;
 }
 
@@ -160,12 +195,12 @@ ul {
   list-style-type: circle;
 }
 
-a {
+li a {
   color: #0056b3;
   text-decoration: none;
 }
 
-a:hover {
+li a:hover {
   color: #0056b3;
   text-decoration: underline;
 }
