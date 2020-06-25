3fea7adf1668b8170b4d6236176abaf83cf236fa
mccrush <mccrush2027@gmail.com>
2020-06-25 17:34:08 +0700

    Add class  stretched-link

--- src/pages/Dev.vue
+++ src/pages/Dev.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="row mt-3">
-    <vueHeadful title="Разработка" description="Ссылки на качественные материалы по веб-разработке. Данная информация бедет полезна не только веб-разработчикам, но и тем, кто желает расширить свой кругозор" />
+    <vueHeadful
+      title="Разработка"
+      description="Ссылки на качественные материалы по веб-разработке. Данная информация бедет полезна не только веб-разработчикам, но и тем, кто желает расширить свой кругозор"
+    />
     <div class="col-12">
       <h3>Разработка</h3>
       <p>Ссылки на качественные материалы по веб-разработке. Данная информация бедет полезна не только веб-разработчикам, но и тем, кто желает расширить свой кругозор</p>
@@ -19,7 +22,11 @@
                   <a href="http://htmlbook.ru/css" target="_blank" title="Открыть сайт">CSS</a>
                 </li>
                 <li>
-                  <a href="https://developer.mozilla.org/ru/docs/Web/JavaScript" target="_blank" title="Открыть сайт">JavaScript | MDN</a>
+                  <a
+                    href="https://developer.mozilla.org/ru/docs/Web/JavaScript"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >JavaScript | MDN</a>
                 </li>
               </ul>
             </div>
@@ -38,10 +45,18 @@
                   <a href="https://vuejs.org/" target="_blank" title="Открыть сайт">Vue.js</a>
                 </li>
                 <li>
-                  <a href="https://firebase.google.com/docs?hl=ru" target="_blank" title="Открыть сайт">Firebase</a>
+                  <a
+                    href="https://firebase.google.com/docs?hl=ru"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >Firebase</a>
                 </li>
                 <li>
-                  <a href="https://codelabs.developers.google.com/codelabs/your-first-pwapp/?hl=ru#0" target="_blank" title="Открыть сайт">PWA (Progressive Web App)</a>
+                  <a
+                    href="https://codelabs.developers.google.com/codelabs/your-first-pwapp/?hl=ru#0"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >PWA (Progressive Web App)</a>
                 </li>
               </ul>
             </div>
@@ -54,10 +69,18 @@
               <h5 class="card-title">Онлайн учебники</h5>
               <ul class="pl-4 mb-1">
                 <li>
-                  <a href="https://learn.javascript.ru/" target="_blank" title="Открыть сайт">Учебник JavaScript</a>
+                  <a
+                    href="https://learn.javascript.ru/"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >Учебник JavaScript</a>
                 </li>
                 <li>
-                  <a href="https://github.com/azat-io/you-dont-know-js-ru" target="_blank" title="Открыть сайт">"Вы не знаете JS"</a>
+                  <a
+                    href="https://github.com/azat-io/you-dont-know-js-ru"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >"Вы не знаете JS"</a>
                 </li>
               </ul>
             </div>
@@ -70,10 +93,18 @@
               <h5 class="card-title">Онлайн курсы</h5>
               <ul class="pl-4 mb-1">
                 <li>
-                  <a href="https://ru.hexlet.io/?ref=107231" target="_blank" title="Открыть сайт">hexlet.io</a>
+                  <a
+                    href="https://ru.hexlet.io/?ref=107231"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >hexlet.io</a>
                 </li>
                 <li>
-                  <a href="https://www.codecademy.com/learn" target="_blank" title="Открыть сайт">codecademy.com (en)</a>
+                  <a
+                    href="https://www.codecademy.com/learn"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >codecademy.com (en)</a>
                 </li>
               </ul>
             </div>
@@ -86,13 +117,25 @@
               <h5 class="card-title">Списки книг</h5>
               <ul class="pl-4 mb-1">
                 <li>
-                  <a title="Открыть сайт" href="https://ru.hexlet.io/pages/recommended-books/?ref=107231" target="_blank">Список hexlet.io</a>
+                  <a
+                    title="Открыть сайт"
+                    href="https://ru.hexlet.io/pages/recommended-books/?ref=107231"
+                    target="_blank"
+                  >Список hexlet.io</a>
                 </li>
                 <li>
-                  <a title="Открыть сайт" href="https://learn.javascript.ru/books" target="_blank">Список learn.javascript.ru</a>
+                  <a
+                    title="Открыть сайт"
+                    href="https://learn.javascript.ru/books"
+                    target="_blank"
+                  >Список learn.javascript.ru</a>
                 </li>
                 <li>
-                  <a title="Открыть сайт" href="https://stackoverflow.com/questions/1711/what-is-the-single-most-influential-book-every-programmer-should-read" target="_blank">stackoverflow.com (en)</a>
+                  <a
+                    title="Открыть сайт"
+                    href="https://stackoverflow.com/questions/1711/what-is-the-single-most-influential-book-every-programmer-should-read"
+                    target="_blank"
+                  >stackoverflow.com (en)</a>
                 </li>
               </ul>
             </div>
@@ -105,16 +148,32 @@
               <h5 class="card-title">YouTube каналы</h5>
               <ul class="pl-4 mb-1">
                 <li>
-                  <a href="https://www.youtube.com/channel/UCg8ss4xW9jASrqWGP30jXiw/playlists" target="_blank" title="Открыть сайт">Владилен Минин</a>
+                  <a
+                    href="https://www.youtube.com/channel/UCg8ss4xW9jASrqWGP30jXiw/playlists"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >Владилен Минин</a>
                 </li>
                 <li>
-                  <a href="https://www.youtube.com/user/ArtSorax/playlists" target="_blank" title="Открыть сайт">Sorax</a>
+                  <a
+                    href="https://www.youtube.com/user/ArtSorax/playlists"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >Sorax</a>
                 </li>
                 <li>
-                  <a href="https://www.youtube.com/user/iliakan/playlists" target="_blank" title="Открыть сайт">JavaScript.ru</a>
+                  <a
+                    href="https://www.youtube.com/user/iliakan/playlists"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >JavaScript.ru</a>
                 </li>
                 <li>
-                  <a href="https://www.youtube.com/user/agragregra/playlists" target="_blank" title="Открыть сайт">WebDesign Master</a>
+                  <a
+                    href="https://www.youtube.com/user/agragregra/playlists"
+                    target="_blank"
+                    title="Открыть сайт"
+                  >WebDesign Master</a>
                 </li>
               </ul>
             </div>
@@ -125,7 +184,11 @@
       <h5>Цикл видео о сетевых технологиях от Дмитрия Бачило</h5>
       <p>
         Сайт:
-        <a title="Перейти на сайт" href="http://16-bits.ru/myvideo/nettech/" target="_blank">16-bits.ru/myvideo/nettech/</a>
+        <a
+          title="Перейти на сайт"
+          href="http://16-bits.ru/myvideo/nettech/"
+          target="_blank"
+        >16-bits.ru/myvideo/nettech/</a>
       </p>
 
       <h5>Список YouTube каналов от Андрея Романова</h5>
@@ -139,31 +202,64 @@
       <div class="row">
         <div class="col-12 col-sm-4">
           <div class="card shadow mb-3 card-pop">
-            <img src="data:image/gif;base64,R0lGODlhHQAQAIAAAP///wAAACH5BAEAAAEALAAAAAAdABAAAAITjI+py+0Po5y02ouz3rz7D4ZhAQA7" data-src="/img/dev/vscode.jpg" class="lazy card-img-top" alt="Visual Studio Code" />
+            <img
+              src="data:image/gif;base64,R0lGODlhHQAQAIAAAP///wAAACH5BAEAAAEALAAAAAAdABAAAAITjI+py+0Po5y02ouz3rz7D4ZhAQA7"
+              data-src="/img/dev/vscode.jpg"
+              class="lazy card-img-top"
+              alt="Visual Studio Code"
+              width="100%"
+              height="150"
+            />
             <div class="card-body">
               <h5 class="card-title">VS Code</h5>
               <p class="card-text">Free. Built on open source. Runs everywhere</p>
-              <a href="https://code.visualstudio.com/" class="btn btn-block btn-sm btn-primary" target="_blank">Перейти</a>
+              <a
+                href="https://code.visualstudio.com/"
+                class="btn btn-block btn-sm btn-primary stretched-link"
+                target="_blank"
+              >Перейти</a>
             </div>
           </div>
         </div>
         <div class="col-12 col-sm-4">
           <div class="card shadow mb-3 card-pop">
-            <img src="data:image/gif;base64,R0lGODlhHQAQAIAAAP///wAAACH5BAEAAAEALAAAAAAdABAAAAITjI+py+0Po5y02ouz3rz7D4ZhAQA7" data-src="/img/dev/storm.jpg" class="lazy card-img-top" alt="WebStorm" />
+            <img
+              src="data:image/gif;base64,R0lGODlhHQAQAIAAAP///wAAACH5BAEAAAEALAAAAAAdABAAAAITjI+py+0Po5y02ouz3rz7D4ZhAQA7"
+              data-src="/img/dev/storm.jpg"
+              class="lazy card-img-top"
+              alt="WebStorm"
+              width="100%"
+              height="150"
+            />
             <div class="card-body">
               <h5 class="card-title">WebStorm</h5>
               <p class="card-text">The smartest JavaScript IDE</p>
-              <a href="https://www.jetbrains.com/webstorm/" class="btn btn-block btn-sm btn-primary" target="_blank">Перейти</a>
+              <a
+                href="https://www.jetbrains.com/webstorm/"
+                class="btn btn-block btn-sm btn-primary stretched-link"
+                target="_blank"
+              >Перейти</a>
             </div>
           </div>
         </div>
         <div class="col-12 col-sm-4">
           <div class="card shadow mb-3 card-pop">
-            <img src="data:image/gif;base64,R0lGODlhHQAQAIAAAP///wAAACH5BAEAAAEALAAAAAAdABAAAAITjI+py+0Po5y02ouz3rz7D4ZhAQA7" data-src="/img/dev/sublime.jpg" class="lazy card-img-top" alt="Sublime Text 3" />
+            <img
+              src="data:image/gif;base64,R0lGODlhHQAQAIAAAP///wAAACH5BAEAAAEALAAAAAAdABAAAAITjI+py+0Po5y02ouz3rz7D4ZhAQA7"
+              data-src="/img/dev/sublime.jpg"
+              class="lazy card-img-top"
+              alt="Sublime Text 3"
+              width="100%"
+              height="150"
+            />
             <div class="card-body">
               <h5 class="card-title">Sublime Text</h5>
               <p class="card-text">A sophisticated text editor for code, markup and prose</p>
-              <a href="https://www.sublimetext.com/" class="btn btn-block btn-sm btn-primary" target="_blank">Перейти</a>
+              <a
+                href="https://www.sublimetext.com/"
+                class="btn btn-block btn-sm btn-primary stretched-link"
+                target="_blank"
+              >Перейти</a>
             </div>
           </div>
         </div>
@@ -173,21 +269,21 @@
 </template>
 
 <script>
-import vueHeadful from "vue-headful";
-import LazyLoad from "vanilla-lazyload";
+import vueHeadful from 'vue-headful'
+import LazyLoad from 'vanilla-lazyload'
 
 export default {
-  name: "dev",
+  name: 'dev',
   components: {
     vueHeadful
   },
   mounted() {
     const lazyContent = new LazyLoad({
-      elements_selector: "img.lazy",
+      elements_selector: 'img.lazy',
       use_native: true // <-- there you go
-    });
+    })
   }
-};
+}
 </script>
 
 <style scoped>
