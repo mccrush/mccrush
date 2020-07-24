3bb23a0c75bee8c40d1a4dccbf24d2368d3a5f90
mccrush <mccrush2027@gmail.com>
2020-07-24 15:28:23 +0700

    Config adaptive props

--- src/pages/Gtd.vue
+++ src/pages/Gtd.vue
@@ -12,7 +12,7 @@
       <hr />
 
       <h5 class="mb-3">Ссылки на полезные сервисы</h5>
-      <div class="row row-cols-1 row-cols-sm-3 g-4">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
         <div class="col">
           <div class="card border-0 shadow h-100">
             <img
