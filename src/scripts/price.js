1f48a317d1ef5f3d7eebdbbe69a1be2e7655a8ea
mccrush <mccrush2027@gmail.com>
2020-08-02 11:31:54 +0700

    Create Price data

---
+++ src/scripts/price.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    title: 'Панель управления',
+    description: 'Админ-панель',
+    price: 7500
+  },
+  {
+    id: 2,
+    title: 'Верстка 1 страницы',
+    description: 'Кроме Лендинга',
+    price: 1200
+  },
+  {
+    id: 3,
+    title: 'Подготовка к SEO',
+    description: 'Поисковая оптимизация',
+    price: 2500
+  },
+  {
+    id: 4,
+    title: 'PWA версия',
+    description: 'Офлайн работа',
+    price: 3500
+  },
+  {
+    id: 5,
+    title: 'Метрики',
+    description: 'Яндекс, Google',
+    price: 1200
+  },
+  {
+    id: 6,
+    title: 'Чат для сайта',
+    description: 'Jivo',
+    price: 800
+  },
+  {
+    id: 7,
+    title: 'Лендинг пейдж',
+    description: 'Посадочная страница',
+    price: 5000
+  },
+]
