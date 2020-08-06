9f699705bf937cbae340cfc335e6f34d4bc2bcd9
mccrush <mccrush2027@gmail.com>
2020-08-06 11:06:56 +0700

    Create local apps db

---
+++ src/scripts/apps.js
@@ -0,0 +1,115 @@
+export default [
+  {
+    title: 'Simpin',
+    alias: 'simpin',
+    description: `<p>Simpin - это приложение для создания простых инструкций. Один шаг - одно простое действие. Создавайте и делитесь инструкциями с друзьями.</p>',
+    type: 'веб-приложение`,
+    version: '1.0.3',
+    github: 'https://github.com/mccrush/simpin',
+    link: 'https://simpin.ru/',
+    position: 1,
+    'date-update': '21.03.2020',
+    'button-text': 'Открыть',
+    img: ''
+  },
+  {
+    title: 'Часы Google Chrome ™',
+    alias: 'clockgooglechrome',
+    description: `<p>Офлайн-приложение. Красивые часы для вашего браузера.</p>
+    <p><strong>Что нового в версии 3.1.0:</strong><br>
+    - Возможность установки собственного фонового изображения;<br>
+    - Будильник;<br>
+    - Секундомер;<br>
+    - Таймер;<br>
+    - Обновлены и добавлены фоновые изображения;<br>
+    - Изменен внешний вид настроек приложения;<br>
+    - Добавлены дополнительные настройки отображения времени и даты;<br>
+    - Устранены мелкие ошибки и добавлены новые.</p>`,
+    type: 'приложение Google Chrome',
+    version: '3.1.0',
+    github: 'https://github.com/mccrush/clockgooglechrome',
+    link: 'https://chrome.google.com/webstore/detail/%D1%87%D0%B0%D1%81%D1%8B-google-chrome/mgncpkiipngfoidimibillghfiplnede',
+    position: 2,
+    'date-update': '18.09.2015',
+    'button-text': 'Установить',
+    img: ''
+  },
+  {
+    title: 'Тест Струпа',
+    alias: 'teststrup',
+    description: `<p>Эффект Струпа – это задержка реакции при прочтении слов, когда цвет слова не совпадает с написанными словами (слово «красный» написано синим).</p>
+    <p>Тест Струпа развивает навыки восприятия.</p>
+    <p><strong>Что нового в версии 3.0.1:</strong><br>
+    - обновленный дизайн;<br>
+    - повышена производительность и стабильность работы;<br>
+    - добавлен режим offline работы.</p>`,
+    type: 'веб-приложение',
+    version: '3.0.1',
+    github: 'https://github.com/mccrush/teststrup',
+    link: 'https://mccrush.github.io/teststrup/',
+    position: 3,
+    'date-update': '06.06.2020',
+    'button-text': 'Открыть',
+    img: ''
+  },
+  {
+    title: 'PomoTime',
+    alias: 'pomotime',
+    description: `<p>Приложение «PomoTime» помогает сосредоточиться при работе с использованием техники POMODORO. Имеет звуковое оповещение по истечении времени.</p>
+    <p><strong>Что нового в версии 2.1.2:</strong><br>
+    1. Обновлен внешний вид приложения.<br>
+    2. Добавлены новые функции:<br>
+    - отображение/скрытие секунд;<br>
+    - отображение/скрытие таймера при работе;<br>
+    - возможность выбора мелодии сигнала;<br>
+    - возможность сброса значений таймера;<br>
+    - указание количества повторов перед большим перерывом.<br>
+    3. Повышена стабильность работы приложения.</p>`,
+    type: 'приложение Windows',
+    version: '2.1.2',
+    github: 'https://github.com/mccrush/pomotime',
+    link: 'https://firebasestorage.googleapis.com/v0/b/mccrush-8de25.appspot.com/o/app%2Fpomotime%2FPomoTime2.1.2.zip?alt=media&token=2779cbd8-4f70-40a4-8a97-61bff1148046',
+    position: 4,
+    'date-update': '10.09.2018',
+    'button-text': 'Скачать',
+    img: ''
+  },
+  {
+    title: 'Игра Алфавит',
+    alias: 'alphabet',
+    description: `<p>Данное приложение способствует развитию навыков восприятия, концентрации внимания. Необходимо вслух произносить появляющуюся букву и поднимать указанную руку или обе руки. На первый взгляд, это может показаться достаточно простым упражнением, но на деле все оказывается совсем не так.</p>
+
+    <p><strong></strong>Что нового в версии 2.1.1:</strong><br>
+    - обновленный дизайн;<br>
+    - повышена производительность и стабильность работы;<br>
+    - добавлен режим offline работы.</p>`,
+    type: 'веб-приложение',
+    version: '2.1.1',
+    github: 'https://github.com/mccrush/alphabet',
+    link: 'https://mccrush.github.io/alphabet/',
+    position: 5,
+    'date-update': '08.04.2019',
+    'button-text': 'Открыть',
+    img: ''
+  },
+  {
+    title: 'WelcomePage',
+    alias: 'welcomepage',
+    description: `<p>WelcomePage стартовая страница Google Chrome.</p>
+
+    <p>Что нового в версии 2.2.0:<br>
+    -исправлена проблема кодировки;<br>
+    -доработан механизм добавления закладок;<br>
+    -исправлены мелкие ошибки и добавлены новые.</p>
+    
+    <p>Расширенные настройки новой вкладки Google Chrome. Теперь вы можете самостоятельно указывать ссылки на часто посещаемые сайты.</p>`,
+    type: 'приложение Google Chrome',
+    version: '2.2.0',
+    github: 'https://github.com/mccrush/welcomepage',
+    link: 'https://chrome.google.com/webstore/detail/welcomepage/jigianpegfcffonfkgmailhacnccobaj',
+    position: 0,
+    'date-update': '21.09.2018',
+    'button-text': 'Установить',
+    img: ''
+  }
+]
