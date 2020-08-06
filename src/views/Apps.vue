88996e3801f1d13063e73d55605bd64ffde7c71d
mccrush <mccrush2027@gmail.com>
2020-08-06 15:46:45 +0700

    Update Apps page

--- src/views/Apps.vue
+++ src/views/Apps.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="row mt-3">
-    <vueHeadful
-      title="Приложения"
-      description="Некоторые из публичных работ. Полезные и не очень: приложения, сервисы и программы"
-    />
+    <vueHeadful title="Приложения" description="mccrush.ru - бесплатные, Open Source приложения" />
     <div class="col-12">
       <h3>Приложения</h3>
-      <p>Некоторые из публичных работ. Полезные и не очень: приложения, сервисы и программы</p>
+      <p>Бесплатные, Open Source приложения</p>
       <hr />
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
         <AppCard v-for="app in apps" :key="'card-'+app.alias" :app="app" />
