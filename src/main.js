8ff215f9559499e042e55057d014e0b7e8055bd2
mccrush <mccrush2027@gmail.com>
2020-10-18 10:35:02 +0700

    Remove comments

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
-//import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-//Vue.config.productionTip = false
-
 let app
-
-// if (!app) {
-//   app = new Vue({
-//     router,
-//     render: function (h) { return h(App) }
-//   }).$mount('#app')
-// }
 
 if (!app) {
   app = createApp(App).use(router).mount('#app')
