2ff856729b5a2c98f7a59a7b5bbfc554164fc021
mccrush <mccrush2027@gmail.com>
2021-04-11 17:15:33 +0700

    Update Github logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,30 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-light p-0 pt-2 pb-2 pb-sm-0">
+  <nav class="navbar navbar-light p-0 pt-2 pb-2 pb-sm-0">
     <div class="container-fluid">
-      <router-link class="navbar-brand me-2" to="/">
+      <div class="navbar-brand me-2">
         <img
           src="/img/logo.png"
           width="32"
           height="32"
-          alt="Logo"
+          alt="Logo mccrush.ru"
           class="mb-0 mb-sm-1"
         />
-      </router-link>
-      <span class="d-sm-none m-0 pt-1">
-        <strong>WA: 89069404069</strong>
-      </span>
+      </div>
 
-      <div class="d-none d-sm-flex justify-content-between">
-        <div class="phone d-none d-sm-block pt-1 me-2">
+      <div class="d-flex justify-content-between">
+        <div class="phone d-block pt-2 me-2">
           <span class="fw-500">WA: 89069404069</span>
         </div>
 
         <a
-          class="mr-0 d-none d-sm-block"
+          class="me-0 d-block"
           href="https://github.com/mccrush/"
           target="_blank"
         >
           <img
-            src="/img/github_32.png"
-            width="28"
-            height="28"
+            src="/img/github_32_w.png"
+            width="32"
+            height="32"
             alt="Github/mccrush"
             title="Github/mccrush"
             class="mb-1"
