5157e31b5e5833c1a80e8c0063c58e192e65cf3a
mccrush <mccrush2027@gmail.com>
2021-04-12 15:26:10 +0700

    Add Year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="col-12 text-center text-white pt-2 pb-2">
     <p class="small m-0">
-      <small>&copy; {{ new Date().getFullYear() }} mccrush.ru</small
+      <small>&copy; 2010-{{ new Date().getFullYear() }} mccrush.ru</small
       >&nbsp;&nbsp; <small>|</small>&nbsp;&nbsp;
       <span class="cursor-point" @click="email = 'mccrush@mail.ru'"
         ><small>{{ email }}</small></span
